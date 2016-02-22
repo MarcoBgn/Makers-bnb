48a2884edcacb8685234db363487b090ba22e4d3
Yan-Yi Li <liyanyi@gmail.com>
2016-02-22 15:41:31 +0000

    5 can add multiple spaces

--- spec/features/listing_spaces_spec.rb
+++ spec/features/listing_spaces_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-# As a Signed In User,
-# so I can promote my spaces,
-# I would like to list a new space
 feature 'listing spaces' do
+  # As a Signed In User,
+  # so I can promote my spaces,
+  # I would like to list a new space
   scenario 'I can list a new space' do
     visit('/spaces')
     click_button 'List a Space'
@@ -11,13 +11,27 @@ feature 'listing spaces' do
     expect(page.status_code).to eq(200)
   end
 
-# As a User,
-# so I can distinguish my spaces,
-# I would like to be able to name them
-scenario 'I can name a space' do
-  visit('/spaces/new')
-  fill_in('name', :with => 'A beautiful relaxing space')
-  click_button 'List my Space'
-  expect(page).to have_content('A beautiful relaxing space')
-end
+  # As a User,
+  # so I can distinguish my spaces,
+  # I would like to be able to name them
+  scenario 'I can name a space' do
+    visit('/spaces/new')
+    fill_in('name', :with => 'A beautiful relaxing space')
+    click_button 'List my Space'
+    expect(page).to have_content('A beautiful relaxing space')
+  end
+
+  # As a Signed in User
+  # So I can list all my spaces,
+  # I would like to be able to add multiple spaces
+  scenario 'I can list multiple spaces' do
+    visit('/spaces/new')
+    fill_in('name', :with => 'A beautiful relaxing space')
+    click_button 'List my Space'
+    visit('/spaces/new')
+    fill_in('name', :with => 'A horrible stressful space')
+    click_button 'List my Space'
+    expect(page).to have_content('A beautiful relaxing space')
+    expect(page).to have_content('A horrible stressful space')
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,16 +14,16 @@ Capybara.app = MakersBnb
 
 RSpec.configure do |config|
 
-  # config.before(:suite) do
-  #     DatabaseCleaner.strategy = :transaction
-  #     DatabaseCleaner.clean_with(:truncation)
-  #   end
+  config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
 
-  #   config.around(:each) do |example|
-  #     DatabaseCleaner.cleaning do
-  #       example.run
-  #     end
-  #   end
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
   config.include Capybara::DSL
 
   config.expect_with :rspec do |expectations|
